f0a9aa51a4b76005af8c9e163a9abe3a580a44a0
g1gabyt3333 <63312367+g1gabyt3333@users.noreply.github.com>
2020-12-20 13:56:10 -0500

    thkng

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var periods = [1, 2, 3, 4, 5, 6, 7, 8]
 var classes = ["Gym", "Language Arts", "Chemistry", "AP Comp Sci", "Geometry", "Study Hall", "AS I", "French"]
-var day = "A"
+
 
 
 function setPer(){
-    for(var i = 0; i<=7; i++){
+    for(var i = 0; i<=5; i++){
         var getPerCol = document.getElementsByClassName("per")[i].innerText = periods[i];
     }
 }
 
-function setClass(){
+function setClass(day){
 
     for(var i = 0; i<=7; i++){
         var getPerCol = document.getElementsByClassName("class")[i].innerText = classes[i];
@@ -17,10 +17,44 @@ function setClass(){
 
 }
 
-
+setPer();
 function main(){
+    var day = document.getElementsByTagName("input")[0].value.toLocaleUpperCase
     setPer();
     setClass();
 }
 
-main();
+function setTime(){
+    var time = new Date();
+    let hour = time.getHours
+    let minutes = time.getMinutes
+    let seconds = time.getSeconds
+    curTime = hour + ":" + minutes + ":" + seconds;
+    return curTime;
+    // hour = (hour > 12) ? hour-12 : hour;
+    
+}
+
+function formatTime(thing){
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+
+}
+
+function changeTime(){
+    var timeId = document.getElementById("curTime");
+    curTime = setTime();
+    formatTime();
+    timeId.innerText = curTime
+    console.log("success")
+}
+
+setInterval(changeTime(), 1000)
+
+
